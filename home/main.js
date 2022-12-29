45ca361ed0f86066caa685194d301a9d6570ad3a
ARDI6 <ardikrasniqi13@gmail.com>
2022-12-29 09:29:17 +0100

    first commit

---
+++ home/main.js
@@ -0,0 +1,6 @@
+let span = document.getElementsByTagName("span");
+let product = document.getElementsByClassName("product");
+let product_page = Math.ceil(product.length / 4);
+let l = 0;
+let movePer = 25.34;
+let maxMove = 203;
